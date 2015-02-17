28f861f4c0ac9b9565fdf56f3a3b9d2bffd8f1b6
Administrator <admin@ninkip2p.com>
2015-02-17 14:43:45 +0900

    Remove builds

--- platforms/android/ant-build/Ninki-release-unsigned.apk.d
+++
@@ -1,5 +0,0 @@
-/Users/ninki/Ninki/platforms/android/ant-build/Ninki-release-unsigned.apk : \
-/Users/ninki/Ninki/platforms/android/ant-build/Ninki.ap_ \
-/Users/ninki/Ninki/platforms/android/ant-build/classes.dex \
-/Users/ninki/Ninki/platforms/android/CordovaLib/ant-build/classes.jar \
-/Users/ninki/Ninki/platforms/android/libs/com.google.zxing.client.android.captureactivity.jar \
