f58671172a7342bc9088cf2651a3ab5de3a01b46
Administrator <admin@ninkip2p.com>
2015-05-20 17:35:05 +0900

    Code for iOS version 0.50 and Android 0.5.2
    Major release
    - re-engineered signup process
    - major UI rework
    - changes to PIN number entry design
    - improved pair-back to chrome app process
    - performance improvements

---
+++ platforms/wp8/www/cordova_plugins.js
@@ -0,0 +1,41 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/com.ionic.keyboard/www/keyboard.js",
+        "id": "com.ionic.keyboard.keyboard",
+        "clobbers": [
+            "cordova.plugins.Keyboard"
+        ]
+    },
+    {
+        "file": "plugins/com.phonegap.plugins.barcodescanner/www/barcodescanner.js",
+        "id": "com.phonegap.plugins.barcodescanner.BarcodeScanner",
+        "clobbers": [
+            "cordova.plugins.barcodeScanner"
+        ]
+    },
+    {
+        "file": "plugins/com.verso.cordova.clipboard/www/clipboard.js",
+        "id": "com.verso.cordova.clipboard.Clipboard",
+        "clobbers": [
+            "cordova.plugins.clipboard"
+        ]
+    },
+    {
+        "file": "plugins/org.apache.cordova.device/www/device.js",
+        "id": "org.apache.cordova.device.device",
+        "clobbers": [
+            "device"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "com.ionic.keyboard": "1.0.4",
+    "com.phonegap.plugins.barcodescanner": "2.0.0",
+    "com.verso.cordova.clipboard": "0.1.0",
+    "org.apache.cordova.device": "0.2.12"
+}
+// BOTTOM OF METADATA
+});
